85750ff8c2a15009e305dc511d13f094b7305d11
evelynit <nyit001122@gmail.com>
2023-07-16 00:54:58 +0200

    Se han creado la clase Product y Tree

---
+++ src/Tree.java
@@ -0,0 +1,15 @@
+public class Tree extends Product{
+    private double height;
+    public Tree(double price, double height) {
+        super(price);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
